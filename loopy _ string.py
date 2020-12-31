70b752f137ae197ff10498d7cf0577709b52a88c
Isaac3N <theisaac3n@gmail.com>
2020-12-31 12:13:06 +0100

    iterating over words

---
+++ loopy _ string.py
@@ -0,0 +1,7 @@
+# Loopy String
+# Demonstrates the for loop with a string
+word = input("Enter a word: ")
+print("\nHere's each letter in your word:")
+for charac in word:
+ print(charac)
+input("\n\nPress the enter key to exit.")
