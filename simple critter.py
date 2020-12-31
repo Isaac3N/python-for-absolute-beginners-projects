c982e78042208713000ab1cb8e262b099a34911a
Isaac3N <theisaac3n@gmail.com>
2020-12-31 11:46:35 +0100

    class and objects

---
+++ simple critter.py
@@ -0,0 +1,14 @@
+# simple critter
+# demonstrates a basic class and object
+
+class critter(object):
+    # a virtual pet
+    def talk(self):
+        print("hi....I'm an instance of class critter.")
+
+#main
+crit = critter()
+crit.talk()
+
+
+input("\n\n press the enter key to exit." )
