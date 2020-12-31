00250bc8d01a8a43bbbc9fff040b46dc3ee27949
Isaac3N <theisaac3n@gmail.com>
2020-12-31 12:06:39 +0100

    using if conditionals

---
+++ password.py
@@ -0,0 +1,10 @@
+#password
+# dremonsrtrating the if statement
+print ("welcome to suysytem security inc.")
+print (" \t where security is our middle name")
+
+password= input (" Enter your password:")
+if password == "******":
+    print ("access granted")
+
+input("\n\n press the enter key to exit")
