e7b4204d6a6779cce11aebf9ade1222e1317ab64
Isaac3N <theisaac3n@gmail.com>
2020-12-31 12:07:49 +0100

    replacing vowels with g

---
+++ no vowels 2.py
@@ -0,0 +1,15 @@
+message = input ("input a message: ")
+newMessage = ""
+VOWELS = "aeiou"
+
+for letter in VOWELS:
+    if letter not in VOWELS :
+        newMessage += g
+    else:
+        newMessage += letter
+        print("A new string has been created:", newMessage)
+print("\nYour message without vowels is:", newMessage)
+
+input("\n\nPress the enter key to exit.")
+
+    
